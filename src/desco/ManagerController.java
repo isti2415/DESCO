523753d80f6dabe2279e529365d80d3e93575efd
isti2415 <2021462@iub.edu.bd>
2023-04-06 04:07:09 +0600

    all file ok

--- src/desco/ManagerController.java
+++ src/desco/ManagerController.java
@@ -5,29 +5,37 @@
  */
 package desco;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author Istiaqs-PC
+ * @author Dell
  */
 public class ManagerController implements Initializable {
 
     @FXML
-    private AnchorPane pane1;
+    private Pane pane1;
     @FXML
     private TextField customerNameTextField;
     @FXML
@@ -104,49 +112,113 @@ public class ManagerController implements Initializable {
     private Pane pane7;
     @FXML
     private TextArea policyTextArea;
+    
+    private void switchPane(int paneNumber) {
+        pane1.setVisible(false);
+        pane2.setVisible(false);
+        pane3.setVisible(false);
+        pane4.setVisible(false);
+        pane5.setVisible(false);
+        pane6.setVisible(false);
+        pane7.setVisible(false);
+        
+
+        switch (paneNumber) {
+            case 1:
+                pane1.setVisible(true);
+                break;
+            case 2:
+                pane2.setVisible(true);
+                break;
+            case 3:
+                pane3.setVisible(true);
+                break;
+            case 4:
+                pane4.setVisible(true);
+                break;
+            case 5:
+                pane5.setVisible(true);
+                break;
+            case 6:
+                pane6.setVisible(true);
+                break;
+            case 7:
+                pane7.setVisible(true);
+                break;
+        }
+    }
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        switchPane(1);
     }    
 
     @FXML
-    private void viewProfileOnClick(ActionEvent event) {
+    private void viewProfile(ActionEvent event) {
+        switchPane(1);
     }
 
     @FXML
     private void viewPerformAttendOnClick(ActionEvent event) {
+        switchPane(2);
     }
 
     @FXML
     private void viewCustomerComplaintOnClick(ActionEvent event) {
+        switchPane(3);
     }
 
     @FXML
     private void viewContractsOnClick(ActionEvent event) {
+        switchPane(4);
     }
 
     @FXML
     private void checkInvOnClick(ActionEvent event) {
+        switchPane(5);
     }
 
     @FXML
     private void viewTargetsOnClick(ActionEvent event) {
+//        switchPane(6);
     }
 
     @FXML
     private void viewReportsOnClick(ActionEvent event) {
+        switchPane(6);
     }
 
     @FXML
     private void viewPolicyOnClick(ActionEvent event) {
+        switchPane(7);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("companypolicy.txt"));
+            policyTextArea.setWrapText(true);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                policyTextArea.appendText(line + "\n");
+            }
+            bufferedReader.close();
+        } catch (IOException ex) {
+            System.out.println("Error reading file: " + ex.getMessage());
+        }
     }
 
     @FXML
     private void logOutOnClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/desco/login.fxml"));
+            Parent root = loader.load();
+            desco.LoginController loginController = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+        }
     }
 
     @FXML
--- src/desco/meterReaderController.java
+++ src/desco/meterReaderController.java
@@ -5,11 +5,18 @@
  */
 package desco;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
@@ -18,11 +25,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author Istiaqs-PC
+ * @author Dell
  */
 public class meterReaderController implements Initializable {
 
@@ -106,49 +114,116 @@ public class meterReaderController implements Initializable {
     private Pane pane8;
     @FXML
     private TextArea policyTextArea;
+    
+    private void switchPane(int paneNumber) {
+        pane1.setVisible(false);
+        pane2.setVisible(false);
+        pane3.setVisible(false);
+        pane4.setVisible(false);
+        pane5.setVisible(false);
+        pane6.setVisible(false);
+        pane7.setVisible(false);
+        pane8.setVisible(false);
+
+        switch (paneNumber) {
+            case 1:
+                pane1.setVisible(true);
+                break;
+            case 2:
+                pane2.setVisible(true);
+                break;
+            case 3:
+                pane3.setVisible(true);
+                break;
+            case 4:
+                pane4.setVisible(true);
+                break;
+            case 5:
+                pane5.setVisible(true);
+                break;
+            case 6:
+                pane6.setVisible(true);
+                break;
+            case 7:
+                pane7.setVisible(true);
+                break;
+            case 8:
+                pane8.setVisible(true);
+                break;    
+        }
+    }
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        switchPane(1);
     }    
 
     @FXML
     private void viewProfileOnClick(ActionEvent event) {
+        switchPane(1);
     }
 
     @FXML
     private void viewEnergyUsageOnClick(ActionEvent event) {
+        switchPane(2);
     }
 
     @FXML
     private void viewInventoryOnClick(ActionEvent event) {
+        switchPane(3);
     }
 
     @FXML
     private void viewReportsOnClick(ActionEvent event) {
+        switchPane(4);
     }
 
     @FXML
     private void viewPerformaneTargetOnClick(ActionEvent event) {
+        switchPane(5);
     }
 
     @FXML
     private void viewSafetyProceduresOnClick(ActionEvent event) {
+        switchPane(6);
     }
 
     @FXML
     private void viewMeterReordsOnClick(ActionEvent event) {
+        switchPane(7);
     }
 
     @FXML
     private void viewCompanyPolicyOnClick(ActionEvent event) {
+        switchPane(8);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("companypolicy.txt"));
+            policyTextArea.setWrapText(true);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                policyTextArea.appendText(line + "\n");
+            }
+            bufferedReader.close();
+        } catch (IOException ex) {
+            System.out.println("Error reading file: " + ex.getMessage());
+        }
     }
 
     @FXML
     private void logOutOnClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/desco/login.fxml"));
+            Parent root = loader.load();
+            desco.LoginController loginController = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+        }
     }
 
     @FXML
